df1fec900dbce86aca7fc49826f70f1fc991526f
amartinezh@gmail.com <amartinezh@gmail.com>
2021-06-26 08:15:00 -0500

    Script DB 2

--- db/sql-files/script.sql
+++ db/sql-files/script.sql
@@ -1,55 +1,26 @@
-CREATE DATABASE ETLBackEnd;
+CREATE DATABASE smart
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
 
-USE ETLBackEnd;
+CREATE SCHEMA adm
+    AUTHORIZATION postgres;
 
-CREATE TABLE Usuario (
-    id_usuario CHARACTER VARYING(40)  NOT NULL,
-    nombre_u CHARACTER VARYING(40)  NOT NULL,
-    cargo_u CHARACTER VARYING(40)  NOT NULL,
-    CONSTRAINT PK_Usuario PRIMARY KEY (id_usuario)); 
+CREATE TABLE adm.tbl_people
+(
+    id character varying COLLATE pg_catalog."default" NOT NULL,
+    name character varying COLLATE pg_catalog."default",
+    apppassword character varying COLLATE pg_catalog."default",
+    CONSTRAINT key_people_id PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
 
-
-CREATE TABLE Carga (
-    id_usuario CHARACTER VARYING(40)  NOT NULL,
-    id_archivo CHARACTER VARYING(40)  NOT NULL,
-    fecha_carga DATE  NOT NULL,
-    id_registro CHARACTER VARYING(40)  NOT NULL,
-    id_maquina CHARACTER VARYING(40)  NOT NULL,
-    CONSTRAINT PK_Carga PRIMARY KEY (id_usuario, id_archivo, id_registro, id_maquina));
-
-
-
-ALTER TABLE Carga ADD CONSTRAINT Usuario_Carga 
-    FOREIGN KEY (id_usuario) REFERENCES Usuario (id_usuario);
-ALTER TABLE Carga ADD CONSTRAINT Archivo_Carga 
-    FOREIGN KEY (id_archivo) REFERENCES Archivo (id_archivo);      
-
-
-CREATE TABLE Archivo (
-    id_archivo CHARACTER VARYING(40)  NOT NULL,
-    fecha_creacion DATE  NOT NULL,
-    descripcion CHARACTER VARYING(200),
-    CONSTRAINT PK_Archivo PRIMARY KEY (id_archivo));
-
-CREATE TABLE Registro (
-    id_registro CHARACTER VARYING(40)  NOT NULL,
-    fecha_registro DATE  NOT NULL,
-    potencia CHARACTER VARYING(40)  NOT NULL,
-    rendimiento CHARACTER VARYING(40)  NOT NULL,
-    consumo CHARACTER VARYING(40)  NOT NULL,
-    id_maquina INTEGER  NOT NULL,
-    id_archivo INTEGER  NOT NULL,
-    CONSTRAINT PK_Registro PRIMARY KEY (id_registro, id_maquina, id_archivo));
-
-ALTER TABLE Registro ADD CONSTRAINT Maquina_Registro 
-    FOREIGN KEY (id_maquina) REFERENCES Maquina (id_maquina);
-ALTER TABLE Registro ADD CONSTRAINT Archivo_Registro 
-    FOREIGN KEY (id_archivo) REFERENCES Archivo (id_archivo);         
-
-
-
-CREATE TABLE Maquina (
-    id_maquina CHARACTER VARYING(40)  NOT NULL,
-    modelo_maquina CHARACTER VARYING(50)  NOT NULL,
-    tipo_maquina CHARACTER VARYING(50)  NOT NULL,
-    CONSTRAINT PK_Maquina PRIMARY KEY (id_maquina)); 
+ALTER TABLE adm.tbl_people
+    OWNER to postgres;
